7636f5ea28ec5da8abe41ffdc33d0fd065ce1561
Elia-Lejzerowicz <eel2157@columbia.edu>
2022-10-02 17:13:59 -0400

    changes

--- src/Data_analysis.py
+++ src/Data_analysis.py
@@ -1,12 +1,5 @@
 import pandas as pd
 import os
-import csv
 
-#f = open("/Users/elialejzerowicz/climate/ML-Climate-Project-Template-Fall2022/Data_website.csv")
-
-#data_website = pd.read_csv("Data_website.csv")
-
-with open('/Users/elialejzerowicz/climate/ML-Climate-Project-Template-Fall2022/Data_website.csv','r') as file:
-    employee = csv.reader(file)
-
-#print(data_website.head(10))
+data_website = pd.read_csv("Data_website.csv")
+print(data_website.head(10))
